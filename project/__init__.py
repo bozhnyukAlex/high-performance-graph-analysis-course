e19437143241399c8a1bc594e012631d7e9289c5
Alexander Bozhnyuk <alexander.bozhnyuk@jetbrains.com>
2022-04-22 16:25:17 +0300

    [Task 3] Fix CI

--- project/__init__.py
+++ project/__init__.py
@@ -1,2 +1,8 @@
+import project.matrix_bfs
+from project.matrix_bfs import *
+
 import project.matrix_ssp
 from project.matrix_ssp import *
+
+import project.triangle_cnt
+from project.triangle_cnt import *
